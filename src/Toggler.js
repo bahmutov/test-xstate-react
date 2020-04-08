909d8d9127993e0b7ec61b32773ceac4b99e0d4c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-08 17:26:46 -0400

    initial toggler code

---
+++ src/Toggler.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useMachine } from '@xstate/react';
+import { Machine } from 'xstate';
+
+const toggleMachine = Machine({
+  id: 'toggle',
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: { TOGGLE: 'active' }
+    },
+    active: {
+      on: { TOGGLE: 'inactive' }
+    }
+  }
+});
+
+export const Toggler = () => {
+  const [state, send] = useMachine(toggleMachine);
+
+  return (
+    <button onClick={() => send('TOGGLE')}>
+      {state.value === 'inactive'
+        ? 'Click to activate'
+        : 'Active! Click to deactivate'}
+    </button>
+  );
+};
